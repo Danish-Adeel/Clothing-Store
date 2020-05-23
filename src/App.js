bd82e79fcfee5c61e2122506d9226c27a48a5b60
Danish-Adeel <danishadeel95@yahoo.com>
2020-05-23 04:30:38 +0200

    divided homepage to components and added styling to menuitems

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./homepage.style.scss";
+import "./pages/homepage/homepage.style.scss";
 import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
