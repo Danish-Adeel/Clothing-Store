9d8a0428cc1c506d24f10f89611bb905dba2f41f
Danish-Adeel <danishadeel95@yahoo.com>
2020-07-02 13:34:37 +0200

    added cart Component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import usereReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
   user: usereReducer,
+  cart: cartReducer,
 });
